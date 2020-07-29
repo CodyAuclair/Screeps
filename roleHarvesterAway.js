102302b27bfc34e921d782414679d72dd7e85113
Cody <cody.auclair@charter.net>
2020-07-28 23:41:00 -0700

    Testing

--- roleHarvesterAway.js
+++ roleHarvesterAway.js
@@ -18,6 +18,20 @@
 // Deliver energy/fulfil roles as needed until empty
 // Repeat
 
-module.exports = {
+var mineUntilFull = require('method_mineUntilFull');
+var deliverToSpawn = require('method_deliverToSpawn');
+var deliverToExtension = require('method_deliverToExtension');
+var deliverToStorage = require('method_deliverToStorage');
+var deliverToTower = require('method_deliverToTower');
+var setStorageFullFlag = require('method_setStorageFullFlag');
+
+var utility = require('utils');
+
+var roleBuilder = require('roleBuilder');
+var roleUpgrader = require('roleUpgrader');
+
+var roleHarvesterAway = {
     
-};
+}
+
+module.exports = roleHarvesterAway;
